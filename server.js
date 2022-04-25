9459db1fba9daad1fb8cf3487fae2960051a6b95
@artanberishaa <artaanberishaa@gmail.com>
2022-04-25 06:36:32 +0200

    Section 1

--- server.js
+++ server.js
@@ -1,17 +1,60 @@
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+const fsPromises = require('fs').promises ;
+
 const logEvents = require('./logEvents');
-
 const EventEmitter = require('events');
-
-class MyEmitter extends EventEmitter {};
-
+class Emitter extends EventEmitter {};
 // initialize object
+const myEmitter = new Emitter();
 
-const myEmitter = new MyEmitter();
+const PORT = process.env.PORT || 3500;
 
-//add listener for the log event
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method);
+
+   const extension = path.extname(req.url);
+
+   let contentType;
+
+   switch (extension) {
+       case '.css':
+            contentType = 'text/css';
+            break;
+        case '.js':
+            contentType = 'text/javascript';
+             break;
+        case '.json':
+             contentType = 'application/json';
+             break;
+        case '.jpeg':
+             contentType = 'image/jpeg';
+             break;
+        case '.png':
+             contentType = 'image/png';
+             break;
+        case '.txt':
+             contentType = 'text/plain';
+             break;
+        default :
+        contentType='text/html';
+   }
+
+   
+});
+
+server.listen(PORT, () => console.log
+(`Server running on port ${PORT}`));
+
+
+
+
+
+
+
+
+/*
 myEmitter.on('log', (msg) => logEvents(msg));
 
-setTimeout(() => {
-    // Emit event
-    myEmitter.emit('log', 'Log event emitted!');
-}, 2000);
+    myEmitter.emit('log', 'Log event emitted!'); */
