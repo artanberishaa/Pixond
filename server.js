ce52ad4c2e3c35ddf71e56557e43a7d4ea11d667
@artanberishaa <artaanberishaa@gmail.com>
2022-04-26 02:06:55 +0200

    Section 2 Middleware

--- server.js
+++ server.js
@@ -1,60 +1,86 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 const path = require('path');
-const fs = require('fs');
-const fsPromises = require('fs').promises ;
-
-const logEvents = require('./logEvents');
-const EventEmitter = require('events');
-class Emitter extends EventEmitter {};
-// initialize object
-const myEmitter = new Emitter();
-
+const cors = require ('cors');
+const { logger } = require('./middleware/logEvents');
+const errorHandler = require('./middleware/errorHandler');
+const res = require('express/lib/response');
 const PORT = process.env.PORT || 3500;
 
-const server = http.createServer((req, res) => {
-    console.log(req.url, req.method);
+const whitelist = ['https://yoursite.com', 'http://127.0.0.1:5500', 'http://localhost:3500'];
+const corsOptions ={
+    origin: (origin, callback) => {
+        if(whitelist.indexOf(origin) !==-1 || !origin) {
+            callback(null, true)
+        } else {
+            callback(new Error ('Not allowed by CORS'));
+        }
+    },
+        optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions));
+// built-in middleware to handle urlencoded data
+// in other words, form data:
+//'content-type: application/x-www-form-urlencoded'
+app.use(express.urlencoded({ extended: false}));
 
-   const extension = path.extname(req.url);
+//built-in middleware for json
+app.use(express.json());
 
-   let contentType;
+//serve static files
+app.use(express.static(path.join(__dirname, 'views', 'index.html')));
 
-   switch (extension) {
-       case '.css':
-            contentType = 'text/css';
-            break;
-        case '.js':
-            contentType = 'text/javascript';
-             break;
-        case '.json':
-             contentType = 'application/json';
-             break;
-        case '.jpeg':
-             contentType = 'image/jpeg';
-             break;
-        case '.png':
-             contentType = 'image/png';
-             break;
-        case '.txt':
-             contentType = 'text/plain';
-             break;
-        default :
-        contentType='text/html';
-   }
-
-   
+app.get('^/$|/index.(html)?' , (req, res) => {
+    //res.sendFile('./views/index.html', {root: __dirname});
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+    
 });
 
-server.listen(PORT, () => console.log
-(`Server running on port ${PORT}`));
+app.get('/new-page(.html)?' , (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
+});
+app.get('/old-page(.html)?' , (req, res) => {
+    res.redirect(301, '/new-page.html'); //302 by default
+});
+
+// Route handlers
+app.get('/hello(.html)?', (req, res, next) => {
+    console.log('attempted to load hello.html');
+    next()
+}, (req, res) => {
+    res.send('Hello World!');
+});
+
+//chaining route handlers
+const one = (req, res, next) => {
+    console.log('one');
+    next();
+}
+const two = (req, res, next) => {
+    console.log('two');
+    next();
+}
+const three = (req, res ) => {
+    console.log('three');
+    res.send('Finished!');
+}
+
+app.get('/chain(.html)?', [one,two, three]);
+
+app.all('*', (req, res) => {
+    res.status(404);
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    } else if (req.accepts('json')) {
+        res.json({"error": "404 Not Found"});
+    } else {
+        res.type('txt').send("404 Not Found");
+    }
+});
+
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 
-
-
-
-
-
-
-/*
-myEmitter.on('log', (msg) => logEvents(msg));
-
-    myEmitter.emit('log', 'Log event emitted!'); */
+    
